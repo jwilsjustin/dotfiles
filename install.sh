b6431d0a860eda660ab2a1c155c6eddd62302b34
Justin Wilson <justin@moonclerk.com>
2023-04-07 11:34:02 -0400

    document command to create nativefier app for devdocs.io

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
+brew bundle --file=./Brewfile
+
 ./copy-to-local
 
 # ln -sv /Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl /usr/local/bin
 
-brew bundle --file=./Brewfile
+# nativefier --disable-old-build-warning-yesiknowitisinsecure https://devdocs.io /Applications
